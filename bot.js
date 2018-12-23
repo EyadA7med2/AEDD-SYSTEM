11b27288bf72ff7adac4b4bf45613a211ef0f7c7
EyadA7med2 <43893562+EyadA7med2@users.noreply.github.com>
2018-12-23 22:50:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 client.on('ready', () => {
-    client.user.setGame(`New Name Vilo Community`, "http://twitch.tv/Streammingg")    
+    client.user.setGame(`AEDD COMMUNITY`, "http://twitch.tv/Streammingg")    
 });
 
 client.on('message', message => {
